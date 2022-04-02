bb7325515f40bd6d87aa1f4d427a41e2ed988d79
Dranikf <kobfedsur@gmail.com>
2022-04-02 09:08:00 +0300

    нелинейная классификация nn готово

--- prog/why logit is linear/common.py
+++ prog/why logit is linear/common.py
@@ -1,8 +1,21 @@
 from sklearn.metrics import roc_curve
+
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 def get_p_bar(Y, probs_hat):
     fpr, tpr, thresholds = roc_curve(Y, probs_hat)
 
     # вот он p-штрих как максимальное
     return thresholds[np.argmax(np.abs(fpr - tpr))]
+
+
+def draw_my_scatter(X1,X2,Y, legend_setting = ['класс1', 'класс2']):
+    '''Отрисовка базового точечного графика с распределением по классам'''
+    plt.scatter(X1[np.invert(Y)], X2[np.invert(Y)], color = 'blue')
+    plt.scatter(X1[Y], X2[Y], color = 'red')
+
+    plt.xlabel('$x_1$', fontsize = 15)
+    plt.ylabel('$x_2$', fontsize = 15)
+    plt.legend(legend_setting, fontsize = 14)
